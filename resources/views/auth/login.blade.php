19b8831e52fa8edc807868b80e89d9b3a127ec50
aGuizada <arminguizada@gmail.com>
2024-08-01 14:35:18 -0400

    nuevos cambios

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,23 +1,15 @@
 @extends('auth.contenido')
 
 @section('login')
-
-
 <div class="content">
   <div class="container-login">
     <div class="left">
       <form class="formulario" method="POST" action="{{ route('login')}}">
         {{ csrf_field() }}
-        <br>
-        <br>
-
         <div class="container-title">
           <h1>Bienvenido</h1>
           <p>Inicia sesión para continuar</p>
         </div>
-        <br>
-        <br>
-
         <div class="container-input">
           <input type="text" value="{{old('usuario')}}" name="usuario" id="usuario" class="input-texto-arriba" placeholder="Usuario">
           <div class="message">
@@ -30,39 +22,35 @@
             {!!$errors->first('password','<span class="invalid-feedback">El campo Contraseña es obligatorio</span>')!!}
           </div>
         </div>
-        <br>
         <div class="container-input">
-          <button type="submit">Ingresar</button>
+          <button type="submit" class="btn-ingresar">iniciar sesion</button>
         </div>
       </form>
     </div>
     <div class="right">
-      <img class="portada" src="img/logomates.png" alt="">
+      <div class="logo-container">
+        <img class="portada" src="img/toolboox.png" alt="Daccar Logo">
+      </div>
     </div>
   </div>
 </div>
 <script>
-  // Escuchamos el evento input en los campos de input
   const inputs = document.querySelectorAll('.input-texto-arriba');
 
   inputs.forEach(input => {
     input.addEventListener('focus', () => {
-      // Agregamos la clase 'texto-arriba' cuando el campo tiene el foco
       input.classList.add('texto-arriba');
     });
 
     input.addEventListener('blur', () => {
-      // Removemos la clase 'texto-arriba' solo si el campo está vacío
-      if (input.value) {
+      if (!input.value) {
         input.classList.remove('texto-arriba');
       }
     });
 
-    // Verificamos si el campo ya tiene un valor inicial al cargar la página
     if (input.value) {
       input.classList.add('texto-arriba');
     }
   });
 </script>
-
 @endsection
