7e2a08d1d85eea65de41a84dfc51882a812251ef
arminguizada69 <arminguizada69@gmail.com>
2025-05-09 00:44:45 -0400

    reporte de ventas se aumento vendedor al reporte

--- resources/views/pdf/ventas_pdf.blade.php
+++ resources/views/pdf/ventas_pdf.blade.php
@@ -21,33 +21,37 @@
         <p>Desde: <strong>{{ $fechaInicio }}</strong> Hasta: <strong>{{ $fechaFin }}</strong></p>
     </div>
     <table>
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Fecha</th>
-                <th>Cliente</th>
-                <th>Comprobante</th>
-                <th>Total</th>
-                <th>Estado</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($ventas as $i => $venta)
-            <tr>
-                <td>{{ $i+1 }}</td>
-                <td>{{ \Carbon\Carbon::parse($venta->fecha_hora)->format('d/m/Y H:i') }}</td>
-                <td>{{ $venta->cliente->nombre ?? 'N/A' }}</td>
-                <td>{{ $venta->tipo_comprobante }} {{ $venta->serie_comprobante }}-{{ $venta->num_comprobante }}</td>
-                <td>{{ number_format($venta->total, 2) }}</td>
-                <td>{{ $venta->estado }}</td>
-            </tr>
-            @endforeach
-            <tr>
-                <td colspan="4" class="total" style="text-align:right;">TOTAL</td>
-                <td class="total">{{ number_format($total, 2) }}</td>
-                <td></td>
-            </tr>
-        </tbody>
+    <thead>
+    <tr>
+        <th>#</th>
+        <th>Fecha</th>
+        <th>Cliente</th>
+        <th>Comprobante</th>
+        <th>Vendedor</th> <!-- Nueva columna -->
+        <th>Total</th>
+        <th>Estado</th>
+    </tr>
+</thead>
+<tbody>
+    @foreach($ventas as $i => $venta)
+    <tr>
+        <td>{{ $i+1 }}</td>
+        <td>{{ \Carbon\Carbon::parse($venta->fecha_hora)->format('d/m/Y H:i') }}</td>
+        <td>{{ $venta->cliente->nombre ?? 'N/A' }}</td>
+        <td>{{ $venta->tipo_comprobante }} {{ $venta->serie_comprobante }}-{{ $venta->num_comprobante }}</td>
+        <td>
+            {{ $venta->usuario->usuario ?? 'N/A' }}
+        </td>
+        <td>{{ number_format($venta->total, 2) }}</td>
+        <td>{{ $venta->estado }}</td>
+    </tr>
+    @endforeach
+    <tr>
+        <td colspan="5" class="total" style="text-align:right;">TOTAL</td>
+        <td class="total">{{ number_format($total, 2) }}</td>
+        <td></td>
+    </tr>
+</tbody>
     </table>
 </body>
 </html>
